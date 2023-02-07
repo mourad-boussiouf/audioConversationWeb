3f6e7700f2229d75fec9502daa8910e4e022c895
mourad-boussiouf <mourad.boussiouf@laplateforme.io>
2023-02-07 23:18:57 +0100

    test intégration dans vue js sans nuxt pour vérifier d'où vient le retour denied de la tentative de create room twilio

---
+++ conversations-project-nonuxt/src/components/Chat.vue
@@ -0,0 +1,14 @@
+<template>
+  <div id="chat">
+    <h1>Welcome to the Vue chat app<span v-if="nameRegistered">, {{ name }}</span>!</h1>
+    <p>{{ statusString }}</p>
+    <div v-if="!nameRegistered">
+      <input @keyup.enter="registerName" v-model="name" placeholder="Enter your name">
+      <button @click="registerName">Register name</button>
+    </div>
+    <div v-if="nameRegistered && !activeConversation && isConnected">
+      <button @click="createConversation">Join chat</button>
+    </div>
+    <Conversation v-if="activeConversation" :active-conversation="activeConversation" :name="name" />
+  </div>
+</template>
